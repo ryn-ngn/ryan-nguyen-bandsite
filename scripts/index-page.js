b5219bf4bee7ac3884bc5f9e1c849bd55c39fd7a
ryn-ngn <ryan.ngn@outlook.com>
2023-12-15 23:07:39 -0500

    add feature to add new comment

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -9,7 +9,7 @@ const comments = [
     name: "Emilie Beach",
     date: "01/09/2021",
     comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+      "I feel blessed to have seen them in person. What a comment! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
@@ -21,25 +21,19 @@ const comments = [
 
 // helper function to add a newly created HTML element to a parent element
 function addNewElementToParent(newEleType, parentEle, newEleClass, newEleContent) {
-  let childElement = document.createElement(newEleType);
+  const childElement = document.createElement(newEleType);
   childElement.classList.add(newEleClass);
   childElement.innerText = newEleContent;
   parentEle.appendChild(childElement);
 }
 
-const cmtList = document.querySelector(".comments-list");
-
-// loop through each comment
-cmtList.appendChild(document.createElement("hr"));
-for (const [key, show] of Object.entries(comments)) {
-  // create show card div, add class name (.classList.add(""))
-  let commentCard = document.createElement("div");
+// helper function to create content of a comment card
+function createCommentCardContent(commentCard, comment) {
   commentCard.classList.add("comment-card");
-
-  let blockClass = "comment-card__";
+  const blockClass = "comment-card__";
 
   // loop through each key-value pair of a comment object
-  Object.keys(show).forEach((key) => {
+  Object.keys(comment).forEach((key) => {
     let tempField;
 
     // decide which key-value pair is being processed
@@ -47,14 +41,46 @@ for (const [key, show] of Object.entries(comments)) {
     else if (key === "date") tempField = "date";
     else tempField = "comment";
 
-    // create heading for each element, add to show card
+    // create heading for each element, add to comment card
     tempClassName = `${blockClass}heading`;
     addNewElementToParent("h3", commentCard, tempClassName, tempField);
 
-    // create content under each heading, add to show card
+    // create content under each heading, add to comment card
     tempClassName = `${blockClass}${tempField}`;
-    addNewElementToParent("p", commentCard, tempClassName, show[tempField]);
+    addNewElementToParent("p", commentCard, tempClassName, comment[tempField]);
   });
+}
+
+const cmtList = document.querySelector(".comments-list");
+
+// loop through each comment
+for (const [key, comment] of Object.entries(comments)) {
+  // create comment card div, add class name (.classList.add(""))
+  const commentCard = document.createElement("div");
+  createCommentCardContent(commentCard, comment);
   commentCard.appendChild(document.createElement("hr"));
   cmtList.appendChild(commentCard);
+}
+
+function addComment() {
+  const userName = document.getElementById("nameInput").value;
+  const comment = document.getElementById("commentInput").value;
+  const date = new Date().toLocaleDateString({
+    month: "short",
+    day: "short",
+    year: "numeric",
+  });
+  const newComment = {
+    name: userName,
+    date: date,
+    comment: comment,
+  };
+
+  const commentCard = document.createElement("div");
+  createCommentCardContent(commentCard, newComment);
+
+  const separatorLine = document.querySelector(".comments-list__separator-line");
+
+  separatorLine.after(commentCard);
+  commentCard.appendChild(document.createElement("hr"));
 }
